bf59c5785ee14f5cff3d7b55019327879423a3bb
Martin Stoev <martin.y.stoev@gmail.com>
2021-01-05 16:30:33 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/vue-logo.png">
+    <Conveyor cname="Some conveyor" />
+  </div>
+</template>
+
+<script>
+import Conveyor from './components/Conveyor'
+
+export default {
+  name: 'App',
+  components: {
+    Conveyor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Conveyor.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{cname}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Conveyor',
+    props: {
+        cname: String
+    }
+}
+</script>
+
+<style>
+
+</style>
